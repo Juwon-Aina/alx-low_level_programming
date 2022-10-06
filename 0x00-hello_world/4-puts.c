c237a03ea5445e23334d7604a3797db802c90762
Juwonaina <juwonaina58@gmail.com>
2022-10-06 06:20:20 -0700

    yo

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,47 @@
+nclude <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Author: Sammykingx
+ *
+ *     *
+ *
+ *      * Return: Always 0 (Success)
+ *
+ *       */
+
+int main(void)
+
+{
+
+		char string[70] = "\"Programming is like building a multilingual puzzle\"";
+
+			
+
+			puts(string);
+
+
+
+				/* Tried printing it directly */
+
+
+
+				puts("\"Programming is like building a multilingual puzzle\"");
+
+					return (0);
+
+}
+
+Footer
+
+© 2022 GitHub, Inc.
+
+Footer navigation
+
+Terms
